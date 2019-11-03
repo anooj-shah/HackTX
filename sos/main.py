5403846ab7704232b1fb521532ee23c74be678e5
Dylan Theriot <dylantheriot@gmail.com>
2019-11-03 02:20:49 -0600

    updated website and cv

--- sos/main.py
+++ sos/main.py
@@ -12,13 +12,9 @@ def home():
     # return '<h1>Added a user!</h1>'
     return render_template('index.html')
 
-@main.route("/map")
-def map():
-    return render_template('map.html')
-
 @main.route("/volunteer")
 def volunteer():
-    return render_template('volunteer.html')
+    return render_template('map.html')
 
 @main.route("/helpMe")
 def helpMe():
@@ -31,4 +27,4 @@ def updateRescueDB():
     address = request.form['address']
     # user_collection = mongo.db.users
     # user_collection.insert({'name' : name, 'number' : number, 'address' ; address})
-    return redirect('/map')
+    return redirect('/volunteer')
